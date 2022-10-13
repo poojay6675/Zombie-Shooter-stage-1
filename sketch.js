3f2dc83005699cb3dfe3d88160412eea4290c2c6
poojay6675 <78363317+poojay6675@users.noreply.github.com>
2022-10-13 14:28:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,4 @@
-var bg,bgImg;
-var player, shooterImg, shooter_shooting;
 
-
-function preload(){
-  
-  shooterImg = loadImage("assets/shooter_2.png")
-  shooter_shooting = loadImage("assets/shooter_3.png")
-
-  bgImg = loadImage("assets/bg.jpeg")
-
-}
 
 function setup() {
 
@@ -17,17 +6,10 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
 
   //adding the background image
-  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
-bg.addImage(bgImg)
-bg.scale = 1.1
-  
+
 
 //creating the player sprite
-player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
- player.addImage(shooterImg)
-   player.scale = 0.3
-   player.debug = true
-   player.setCollider("rectangle",0,0,300,300)
+
 
 
 }
@@ -39,26 +21,13 @@ function draw() {
 
 
   //moving the player up and down and making the game mobile compatible using touches
-if(keyDown("UP_ARROW")||touches.length>0){
-  player.y = player.y-30
-}
-if(keyDown("DOWN_ARROW")||touches.length>0){
- player.y = player.y+30
-}
+
 
 
 //release bullets and change the image of shooter to shooting position when space is pressed
-if(keyWentDown("space")){
- 
-  player.addImage(shooter_shooting)
- 
-}
+
 
 //player goes back to original standing image once we stop pressing the space bar
-else if(keyWentUp("space")){
-  player.addImage(shooterImg)
-}
 
-drawSprites();
 
 }
